7ef811f68c24551c05846cdddd958cd6207baa5a
natsuoo <oinatsuo@gmail.com>
2024-02-10 02:43:28 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,8 +16,8 @@ overlay.style.cssText = `
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
+    max-width: 50%;
+    max-height: 50%;
     display: none;
     position: absolute;
     top: 50%;
